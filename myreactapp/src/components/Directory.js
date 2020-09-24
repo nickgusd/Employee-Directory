d701e0f76a9479041bde2f1f02066cf850546658
Nick Guimarais <Nick.guimarais@gmail.com>
2020-09-24 15:34:03 -0700

    removed comments

--- myreactapp/src/components/Directory.js
+++ myreactapp/src/components/Directory.js
@@ -11,26 +11,12 @@ const DirectoryList = (props) => {
     <td> DOB: {props.dob}    </td>
     <td> ID: {props.id}    </td>
         </tr>
-//need to replace the td to how we have the divs below
 
-
-
-
-    //             <div className="row">
-    // <div className="col-3-xs"><img alt={props.name} src={props.image} /></div>
-    // <div className="col-2-xs">Phone: {props.phone}</div>
-    // <div className="col-2-xs">Name: {props.first} {props.last}</div>
-    // <div className="col-3-xs">Email {props.email}</div>
-    //                 <div className="col-2-xs">DOB: {props.dob}</div>
-    //             </div>
     )
 }
 
 
-
 function Directory(props) {
-
-
 
     return(
 
@@ -62,73 +48,6 @@ function Directory(props) {
             ))}
   </tbody>
 </table>
-
-
-
-
-
-
-
-
-  {/* <div className="row">
-    <div className="col">
-      Image
-      <ul className="list-group">
-      {props.employees.map(employee => (
-        <li className="list-group-item" key={employee.id.value}>
-          <img alt="employee" src={employee.picture.thumbnail} />
-        </li>
-      ))}
-    </ul>
-    </div>
-
-
-    <div className="col">
-      Phone
-      <ul className="list-group">
-      {props.employees.map(employee => (
-        <li className="list-group-item" key={employee.id.value}>
-          {employee.phone}
-        </li>
-      ))}
-    </ul>
-    </div>
-
-
-    <div className="col">
-      Name
-      <ul className="list-group">
-      {props.employees.map(employee => (
-        <li className="list-group-item" key={employee.id.value}>
-          {employee.name.first} {employee.name.last}
-        </li>
-      ))}
-    </ul>
-    </div>
-
-
-    <div className="col">
-      Email
-      <ul className="list-group">
-      {props.employees.map(employee => (
-        <li className="list-group-item" key={employee.id.value}>
-          {employee.email} 
-        </li>
-      ))}
-    </ul>
-    </div>
-
-    <div className="col">
-     Dob
-     <ul className="list-group">
-      {props.employees.map(employee => (
-        <li className="list-group-item" key={employee.id.value}>
-          {employee.dob.date} 
-        </li>
-      ))}
-    </ul>
-    </div>
-  </div> */}
     </div>
     )
 }
