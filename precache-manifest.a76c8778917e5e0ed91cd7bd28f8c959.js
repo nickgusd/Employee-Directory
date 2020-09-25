e21385c237539ebe39c9cb3318823e8d4d716d85
Nick Guimarais <Nick.guimarais@gmail.com>
2020-09-25 09:47:42 -0700

    Updates

---
+++ precache-manifest.a76c8778917e5e0ed91cd7bd28f8c959.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e69d31ed62756251d862322e2a2037",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "be309705ee0870a9213a",
+    "url": "/Employee-Directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "be309705ee0870a9213a",
+    "url": "/Employee-Directory/static/js/2.768e4578.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.768e4578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f40eb5985c9bd79c47b",
+    "url": "/Employee-Directory/static/js/main.573e677e.chunk.js"
+  },
+  {
+    "revision": "4f1d9d7a274876de7660",
+    "url": "/Employee-Directory/static/js/runtime-main.45c9c6ad.js"
+  }
+]);
